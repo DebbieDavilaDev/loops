78f712d1fe72c189f7734213d011b01178d54ee2
Debbie Davila <debbie@debbiedavila.com>
2023-10-10 10:52:37 -0400

    Demo for how to make a for loop

---
+++ loops.js
@@ -0,0 +1,19 @@
+// Say hello 3 times
+
+
+/*
+0
+1
+2
+3
+*/
+
+
+  
+// make a loop that says hello to each person in the array
+    const people = ['todd', 'andrew', 'derek', 'andy', 'john']
+    for(let i = 0; i < people.length; i++){
+        console.log('hello ' + people[i])
+}
+    
+    
